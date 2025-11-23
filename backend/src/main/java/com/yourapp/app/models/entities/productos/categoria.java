62d9f09e84f5e93883e1bcfbd39ae8418bf26993
aylencandela <aylencandela@hotmail.com>
2025-11-23 14:15:04 -0300

    agrego atributos de commit

--- backend/src/main/java/com/yourapp/app/models/entities/productos/categoria.java
+++ backend/src/main/java/com/yourapp/app/models/entities/productos/categoria.java
@@ -1,5 +1,35 @@
 package com.yourapp.app.models.entities.productos;
 
 public class Categoria {
+    private int id;
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isEstaActiva() {
+        return estaActiva;
+    }
+
+    public void setEstaActiva(boolean estaActiva) {
+        this.estaActiva = estaActiva;
+    }
+
     private String descripcion;
+    private boolean estaActiva;
+
+    // Constructor vacío
+    public Categoria() {}
+
+    // Constructor con parámetros
+    public Categoria(int id, String descripcion, boolean estaActiva) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.estaActiva = estaActiva;
+    }
+
 }
