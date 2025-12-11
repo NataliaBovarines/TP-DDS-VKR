5f0cebbec5f57b8c9a86ab264b2b6eba7c16a401
aylencandela <aylencandela@hotmail.com>
2025-12-11 16:15:36 -0300

    state

--- backend/src/main/java/com/yourapp/app/models/entities/state/VentaState.java
+++ backend/src/main/java/com/yourapp/app/models/entities/state/VentaState.java
@@ -1,11 +1,39 @@
 package com.yourapp.app.models.entities.state;
 
-import com.yourapp.app.models.entities.Venta;
+import com.yourapp.app.models.entities.DetalleVenta;
+import com.yourapp.app.models.entities.Producto;
+import com.yourapp.app.models.entities.reportes.ReporteVenta;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
 public abstract class VentaState {
-    private Venta venta; 
+
+    private List<DetalleVenta> ventas;
     
-    public VentaState(Venta venta) {
-        this.venta = venta;
+    public VentaState (List<DetalleVenta> ventas) {
+        this.ventas = ventas;
+    }
+
+    public Double calcularMontoRestante(Double monto) {
+        Double precioVentas = ventas.stream().map(d  -> d.getPrecioTotalUnitario()).collect(Collectors.summingDouble(Double::doubleValue));
+        return precioVentas - monto;
+    }
+    public int disminuirStock() {
+        for (DetalleVenta detalle : ventas) {
+            Producto producto = detalle.getProducto();
+            int nuevaCantidad = producto.getStockActual() - detalle.getCantidad();
+            producto.setStockActual(nuevaCantidad);
+        }
+        return 0;
+    }
+
+    public ReporteVenta registrar() {
+        //TODO falta ver cómo impacta el registro de una venta y si puede estar en la clase VentaState, ¿Que reporte de venta específico devuelve?
+        return null;
     }
 }
